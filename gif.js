8f216c5c38de374712a286539efa2d75315888aa
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-09-28 17:01:11 -0400

    Added button to generate new gif.
    Users can now get a new gif without refreshing the page.

--- gif.js
+++ gif.js
@@ -1,10 +1,15 @@
 const img = document.querySelector("img");
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=oFYJzNY5lnXdZONrmxJVCCaIib10X1ss&s=cats"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+const button = document.querySelector("button");
+function newGif() {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=oFYJzNY5lnXdZONrmxJVCCaIib10X1ss&s=cats"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+}
+
+button.addEventListener("click", newGif);
